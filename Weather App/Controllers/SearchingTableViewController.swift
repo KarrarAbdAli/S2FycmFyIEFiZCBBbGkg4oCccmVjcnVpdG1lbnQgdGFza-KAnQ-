51fa488cf08c0ec5345e074b476aee11b3e9b7d9
Karrar Abd Ali <>
2020-05-21 11:42:33 +0200

    adding searching View controller

---
+++ Weather App/Controllers/SearchingTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SearchingTableViewController.swift
+//  Weather App
+//
+//  Created by Karrar Abd Ali on 21/05/2020.
+//  Copyright © 2020 Karrar Abd Ali. All rights reserved.
+//
+
+import UIKit
+
+class SearchingTableViewController: UITableViewController {
+    var weatherItems: [WeatherObject] = []
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch searchBar.text {
+        case nil: return weatherItems.count
+        default: return 1
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = weatherItems[indexPath.row].name
+        return cell
+    }
+
+}
